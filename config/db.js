09326f7accab809980ada9378d8abcb90a173a0f
Hafsa Khan Lodhi <hafsalodhi2023@gmail.com>
2024-12-15 22:31:26 +0500

    `Refactor static file serving and MongoDB connection`

--- config/db.js
+++ config/db.js
@@ -2,7 +2,10 @@ const mongoose = require("mongoose");
 
 module.exports = async function () {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log("Connected to MongoDB");
   } catch (error) {
     console.log(error);
