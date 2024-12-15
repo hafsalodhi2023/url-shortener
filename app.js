0bfad9fccc4daaa4f200476a0243653849b11ce4
Hafsa Khan Lodhi <hafsalodhi2023@gmail.com>
2024-12-15 22:43:28 +0500

    `Removed explicit path for serving static files, using default path instead`

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-app.use("/public", express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "public")));
 app.set("view engine", "ejs");
 
 app.use("/url", urlRoute);
