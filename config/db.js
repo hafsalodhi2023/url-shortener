0baa70fc469fb41b5ea8c3194fbcfd9e39e25d1f
Hafsa Khan Lodhi <hafsalodhi2023@gmail.com>
2024-12-16 14:49:43 +0500

    `Update db connection string in config/db.js`

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 module.exports = async function () {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(`${process.env.MONGO_URI}`);
     console.log("Connected to MongoDB");
   } catch (error) {
     console.log(error);
